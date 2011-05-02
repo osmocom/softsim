cbd7c97b726b7e361ce3e5fafaee8cb67d1aa495
Kevin Redon <kevredon@mail.tsaitgaist.info>
2011-05-02 12:34:31 +0200

    add gpl3 license

--- src/lib/apdu.rb
+++ src/lib/apdu.rb
@@ -1,3 +1,22 @@
+=begin
+This file is part of SAP.
+
+SAP is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SAP is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SAP.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2011 Kevin "tsaitgaist" Redon kevredon@mail.tsaitgaist.info
+=end
+
 # transform binary string into readable hex string
 class String
   def to_hex_disp
--- src/pcsc_server.rb
+++ src/pcsc_server.rb
@@ -1,4 +1,22 @@
 #!/usr/bin/env ruby
+=begin
+This file is part of SAP.
+
+SAP is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SAP is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SAP.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2011 Kevin "tsaitgaist" Redon kevredon@mail.tsaitgaist.info
+=end
 require 'lib/server'
 require 'socket'
 require 'rubygems'
